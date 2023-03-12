8a78e508b803c1b03b468d05d9e9c2b609c9ccb7
sedrik13 <122992058+sedrik13@users.noreply.github.com>
2023-03-12 14:49:14 +0300

    add work

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int Balans = 100; //начальный счет
-        int deposit = 1000; // сумма пополнения
-        int bonus =; // бонусы
+        int balans = 100; //начальный счет
+        int deposit = 1100; // сумма пополнения
+        int bonus; // бонусы
         int finalBalans; // итог
 
-        if (deposit > 1_000) {
+        if (deposit > 1000) {
             bonus = deposit / 100;
         } else {
-            bonus = 0;}
-            finalBalans = Balans + deposit + bonus;
-            System.out.println("Итоговый баланс " + finalBalans);
-            System.out.println("Бонусы " + bonus);
+            bonus = 0;
         }
+        finalBalans = balans + deposit + bonus;
+        System.out.println("Итоговый баланс " + finalBalans);
+        System.out.println("Бонусы " + bonus);
     }
+}
